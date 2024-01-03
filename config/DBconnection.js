12a64ce66c53068514ae4efefff353abba97717c
Djumali Osmani <xhumaliosmani@outlook.com>
2024-01-03 22:11:19 +0100

    fixed bug multer

--- config/DBconnection.js
+++ config/DBconnection.js
@@ -3,7 +3,8 @@ const mongoose = require('mongoose');
 const dbconnection = async ()=> {
     try {
         await mongoose.connect(process.env.APP_MONGODB_URL, {
-            useUnifiedTopology: true,
+          useNewUrlParser: true,
+          useUnifiedTopology: true,
         });
         console.log("MongoDB Connected...");
     } catch (error) {
